fd268f0fc58bfa4b7e7255e434d3df139380446c
Adam Haris <iamadamharis@gmail.com>
2020-01-06 18:22:22 +0100

    updates

--- Gemfile
+++ Gemfile
@@ -1,8 +1,10 @@
 source 'https://rubygems.org'
 
-gem "middleman", "~>3.4.0"
-gem "middleman-livereload", "~> 3.1.0"
+gem "middleman"
+gem "middleman-livereload"
 gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
 gem "tzinfo-data", platforms: [:mswin, :mingw, :jruby]
-gem 'font-awesome-middleman'
+gem 'font-awesome-sass', '~> 5.12.0'
+gem 'sass'
+gem 'middleman-sprockets', '~> 4.0'
 gem 'middleman-gh-pages'
--- config.rb
+++ config.rb
@@ -49,24 +49,15 @@
 
 set :css_dir, 'stylesheets'
 
+set :scss
+
 set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
+set :fonts_dir, 'fonts'
+
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
-
-  # Enable cache buster
-  # activate :asset_hash
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+  activate :sprockets
 end
